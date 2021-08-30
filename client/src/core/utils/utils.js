f872a4bfb90432370dc980187dfe634cfc05fb54
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-08-30 06:07:13 -0400

    refactor MapBoard util using reduce

--- client/src/core/utils/utils.js
+++ client/src/core/utils/utils.js
@@ -19,28 +19,30 @@ const FromResize = (element) => {
 }
 
 
-function MapBoard(matrix){
-	let map = {}
+// Input
+//	[[{type:'r',color:'w'}, ...], Array(8), Array(8), ...]
+// Output
+//	{a1: "r_w", a2: "p_w", ...}
+function MapBoard(board){
 	let piecesCount = { 
 		w: {'p':0,'r':0,'n':0,'b':0,'q':0,'k':0},
 		b: {'p':0,'r':0,'n':0,'b':0,'q':0,'k':0}
 	}
-	let cols = ['a','b','c','d','e','f','g','h']
-	matrix.forEach((row, i)=>{
-		row.forEach((piece, j)=>{
-			// console.log('mapping')
-			let sq = cols[j] + (8-i)
-			if(!piece) {
-				map[sq] = null
-				return
+	let files = ['a','b','c','d','e','f','g','h']
+	return board.reduce((rows, row, rIdx) => {
+		let rowSquares = row.reduce((squares, piece, cIdx) => {
+			let pieceId = '';
+			let sq = files[cIdx] + (8-rIdx);
+			if (piece){
+				let count = ++piecesCount[piece.color][piece.type]
+				pieceId = piece.type +'_'+ piece.color + (count > 1 ? '_'+ count : '')
 			}
-			let count = ++piecesCount[piece.color][piece.type]
-			let id = piece.type + '_' + piece.color + (count > 1 ? '_' + count : '')
-			map[sq] = id
-		})
-	})
-	return map
+			return {...squares, [sq]: pieceId}
+		},{})
+		return {...rows, ...rowSquares}
+	}, {});
 }
+
 
 export {
 	MapBoard,
