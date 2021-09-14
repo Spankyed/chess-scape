0cfbb893c62a07b8c8bd8b34b2506b31f65765e2
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-09-14 08:45:18 -0400

    allow multiple alerts at once

--- client/src/ui/Shared/Alert.js
+++ client/src/ui/Shared/Alert.js
@@ -1,78 +1,107 @@
 import { h } from 'hyperapp';
+import { nanoid } from 'nanoid/non-secure'
 
+/*
+{
+	options: {
+		position: 'top',
+		actions: {positive:{}, negative:{}}
+		showDAG: true, // show Don't ask again checkbox
+		dontAskAgain: true
+	},
+}
+*/
 export default initial => ({
 	state: { 
-		isVisible: false,
-		options: {
-			position: 'top',
-			// actions: {positive:{}, negative:{}}
-			// dontAskAgain: true
-		},
-		showDAG: true, // show Don't ask again checkbox
+		alerts: {},
 	},
 	actions: { 
-		show: options => state => ({
-			isVisible: true, 
-			showDAG: typeof options?.dontAskAgain === 'boolean', 
-			options
-		}),
-		hide: _ => _ => ({isVisible: false}),
-		toggleAskAgain: _ => state => ({options: {...state.options, dontAskAgain: !state.options.dontAskAgain}})
-	},
-	view: ({options, isVisible, showDAG}, actions) => ({gameId, leaveGame}) => { 
-		// todo: reset state ondestroy
-		const handleAction = type => _ => {
-			actions.hide()
-			const result = type == 'positive' ? true : false
-			options.actions?.[type].handler(result, options.dontAskAgain)
-		}
-		return ( 
-		<div class="alert-wrapper h-full absolute top-0 w-full">
-		{	
-		<div class={`alert info mx-auto flex-row justify-between ${!isVisible && 'hidden'}`}>
-			<div class="alert-icon flex items-center bg-blue-100 border-r border-blue-500 justify-center w-20 flex-shrink-0">
-				<span class="text-blue-500 px-3 h-full">
-					<img class="alert-icon-img h-full" src={options.icon}/> 
-				</span>
-			</div>
-			<div class="alert-text m-2 mx-2 md:mx-4 flex-grow relative">
-				<div class="alert-title font-semibold text-gray-800">
-					{options.heading}
-				</div>
-				<div class="alert-message ">
-					{options.message}
-				</div>
-				{	showDAG &&
-					<label class="flex items-center absolute right-0 top-0 text-sm text-gray-500">
-						<input onchange={actions.toggleAskAgain} checked={options.dontAskAgain} class="form-checkbox" type="checkbox"/>
-						<span class="md:hidden ml-1">Remember</span>
-						<span class="hidden md:inline ml-2">Don't ask again</span>
-					</label> 
+		show: options => ({alerts}) => ({
+			alerts: {
+				...alerts,
+				[options.id || nanoid()]: {
+					...options,
+					showDAG: typeof options?.dontAskAgain === 'boolean', 
 				}
-			</div>
-			{	options.actions && 
-				<div class="alert-options flex-shrink flex flex-col font-semibold">
-					{	options.actions.positive &&
-					<button onclick={handleAction('positive')} class="alert-button text-blue-800 p-1 px-4">
-						{options.actions.positive.text}
-					</button>
-					}
-					{	options.actions.negative &&
-					<button onclick={handleAction('negative')} class="alert-button text-gray-800 p-1 px-4">
-						{options.actions.negative.text}
-					</button>
-					}
-					{	options.actions.cancel &&
-					<button onclick={handleAction('cancel')} class="alert-button text-gray-800 p-1 px-4">
-						{options.actions.cancel.text}
-					</button>
-					}
-				</div>
 			}
-		</div>
-		}
-		</div>
+		}),
+		close: (id) => ({alerts}) => {
+			let { [id]: remove, ...rest } = alerts
+			return ({  alerts: { ...rest } })
+		},
+		toggleAskAgain: id => ({alerts}) => ({
+			alerts: {
+				...alerts,
+				[id]: {
+					...alerts[id],
+					dontAskAgain: !alerts[id].dontAskAgain
+				}
+			}
+		})
+	},
+	view: ({alerts}, actions) => ({gameId, leaveGame}) => { 
+		// todo remove alerts when user leaves gameRoom
+		return ( 
+			<div class="alert-wrapper h-full absolute top-0 w-full">
+			{
+				Object.entries(alerts).map(([id, alert])=>
+					<Alert {...{actions, id, alert}}/>
+				)
+			}
+			</div>
 		)
 	}
 })
+
+function Alert({id, alert, actions}){
+		let { close, toggleAskAgain } = actions
+		const handleAction = type => _ => {
+			const result = type == 'positive' ? true : false
+			alert.actions?.[type].handler(result, alert.dontAskAgain)
+			close(id, true)
+		}
+		return (
+			<div class='alert info mx-auto flex-row justify-between'>
+				<div class="alert-icon flex items-center bg-blue-100 border-r border-blue-500 justify-center w-20 flex-shrink-0">
+					<span class="text-blue-500 px-3 h-full">
+						<img class="alert-icon-img h-full" src={alert.icon}/> 
+					</span>
+				</div>
+				<div class="alert-text m-2 mx-2 md:mx-4 flex-grow relative">
+					<div class="alert-title font-semibold text-gray-800">
+						{alert.heading}
+					</div>
+					<div class="alert-message ">
+						{alert.message}
+					</div>
+					{	alert.showDAG &&
+						<label class="flex items-center absolute right-0 top-0 text-sm text-gray-500">
+							<input onchange={toggleAskAgain} checked={alert.dontAskAgain} class="form-checkbox" type="checkbox"/>
+							<span class="md:hidden ml-1">Remember</span>
+							<span class="hidden md:inline ml-2">Don't ask again</span>
+						</label> 
+					}
+				</div>
+				{	alert.actions && 
+					<div class="alert-options flex-shrink flex flex-col font-semibold">
+						{	alert.actions.positive &&
+						<button onclick={handleAction('positive')} class="alert-button text-blue-800 p-1 px-4">
+							{alert.actions.positive.text}
+						</button>
+						}
+						{	alert.actions.negative &&
+						<button onclick={handleAction('negative')} class="alert-button text-gray-800 p-1 px-4">
+							{alert.actions.negative.text}
+						</button>
+						}
+						{	alert.actions.cancel &&
+						<button onclick={handleAction('cancel')} class="alert-button text-gray-800 p-1 px-4">
+							{alert.actions.cancel.text}
+						</button>
+						}
+					</div>
+				}
+			</div>
+		)
+}
 
