efe54345a876416532baf6189726b98c0ddca528
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-09-25 20:00:00 -0400

    add parcel header issue reference

--- client/.proxyrc.js
+++ client/.proxyrc.js
@@ -1,4 +1,5 @@
-module.exports = function (app) {
+// https://stackoverflow.com/a/68664795/8723748
+https: module.exports = function (app) {
 	app.use((req, res, next) => {
 		res.removeHeader("Cross-Origin-Resource-Policy");
 		res.removeHeader("Cross-Origin-Embedder-Policy");
