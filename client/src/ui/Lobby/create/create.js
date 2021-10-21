3b165eb8a89d6c123801691686dfea9d323eda2e
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-10-21 18:21:04 -0400

    refactor lobby/create css from inline tailwind to stylesheets

--- client/src/ui/Lobby/create/create.js
+++ client/src/ui/Lobby/create/create.js
@@ -110,7 +110,7 @@ export default (initial) => ({
 
 				try {
 					actions.attemptSubmit();
-					const { newRoom } = await Api.createRoom(gameOptions); // dont update room list with response, websocket message should be sent to update room list in lobby
+					const { newRoom } = await Api.createRoom(gameOptions); // dont update room list with response, websocket message will be sent to update lobby roomlist
 					if (newRoom) toggleCreate();
 					delay(300).then(actions.endAttempt).then(actions.reset);
 				} catch (err) {
@@ -125,28 +125,21 @@ export default (initial) => ({
 			}
 
 			return (
-				<div
-					class={`${
-						showCreate ? "" : "opacity-0 pointer-events-none"
-					} create-wrapper modal fixed w-full h-full top-0 left-0`}
-				>
+				<div class={`create-wrapper modal ${ !showCreate && "hide-create"}`} >
 					{showCreate && (
-						<div
-							oncreate={checkConnection}
-							class="create flex items-center justify-center"
-						>
+						<div oncreate={checkConnection} class="create">
 							<div
 								onclick={toggle}
-								class="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"
+								class="modal-overlay"
 							></div>
 
-							<div class="bg-white w-11/12 max-w-md mx-auto rounded shadow-lg z-50">
+							<div class="create-body">
 								{/* <div class="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
-							<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-								<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
-							</svg>
-							<span class="text-sm">(Esc)</span>
-						</div> */}
+										<svg class="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+											<path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+										</svg>
+										<span class="text-sm">(Esc)</span>
+									</div> */}
 
 								<ColorSelect
 									{...actions}
@@ -154,9 +147,8 @@ export default (initial) => ({
 								/>
 
 								{/* <!-- Add margin if you want to see some of the overlay behind the modal--> */}
-								<div class="create-content py-4 text-left px-6 overflow-auto">
-									{/* <!--Title--> */}
-									<div class="justify-between items-center grid grid-cols-2 gap-5 w-full">
+								<div class="create-content">
+									<div class="preset-types">
 										{gameTypes.map((type) => (
 											<GameType
 												{...actions}
@@ -187,44 +179,11 @@ export default (initial) => ({
 			);
 		},
 });
-
-function GameType({ type, selectGameType, selectedGameType }) {
-	const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
-	return (
-		<div
-			onclick={() => selectGameType(type.id)}
-			class={`cursor-pointer bg-green-200 py-2 rounded-md ${
-				type.id == selectedGameType && "selected"
-			}`}
-			style="min-height:85px"
-		>
-			<div class="flex">
-				<div class="w-2/3">
-					<h2 class="text-base md:text-md lg:text-md px-4 whitespace-no-wrap text-gray-600">
-						{capitalize(type.name)}
-					</h2>
-					{type.time && (
-						<h3 class="text-base md:text-md lg:text-md px-8 py-2 text-gray-600">
-							{type.time.minutes} min
-						</h3>
-					)}
-				</div>
-				<div class="w-1/3 flex justify-center items-center mr-2">
-					<img
-						src={`./assets/create/types/${type.name}.svg`}
-						alt="game type"
-					/>
-				</div>
-			</div>
-		</div>
-	);
-}
-
 function ColorSelect({ selectedColor, selectColor }) {
 	const colors = ["white", "random", "black"];
 	return (
-		<div class="choose-color cursor-pointer">
-			{colors.map((color, idx) => (
+		<div class="choose-color">
+			{colors.map((color) => (
 				<div
 					class={`piece-color ${
 						color == selectedColor && "selected"
@@ -234,36 +193,47 @@ function ColorSelect({ selectedColor, selectColor }) {
 						id={`color-select-${color}`}
 						onclick={(_) => selectColor(color)}
 						src={`./assets/create/piece-${color}.svg`}
-						style="max-height: 90px;"
-						// style="max-height: 120px;"
 					/>
 				</div>
 			))}
 		</div>
 	);
 }
-
-function Footer({ create, toggle }) {
+function GameType({ type, selectGameType, selectedGameType }) {
+	const capitalize = (s) => s && s[0].toUpperCase() + s.slice(1);
 	return (
 		<div
-			onclick={debounce(create, 400)}
-			class="modal-footer bg-gray-50 pb-3 px-6 flex flex-row-reverse "
+			onclick={() => selectGameType(type.id)}
+			class={`type ${type.id == selectedGameType && "selected"}`}
 		>
+			<div class="time">
+				<h2 class="name">
+					{capitalize(type.name)}
+				</h2>
+				{type.time && (
+					<h3 class="value">
+						{type.time.minutes} min
+					</h3>
+				)}
+			</div>
+			<img
+				src={`./assets/create/types/${type.name}.svg`}
+				alt="game type"
+			/>
+		</div>
+	);
+}
+function Footer({ create, toggle }) {
+	return (
+		<div class="modal-footer">
 			<button
+				onclick={debounce(create, 400)}
 				type="button"
-				class="w-full mt-3 inline-flex justify-center rounded-sm border border-transparent shadow-sm px-4 py-2  
-				text-base font-medium text-white bg-indigo-500 hover:bg-indigo-400 focus:outline-none focus:ring-2
-				focus:ring-offset-2 focus:ring-red-500 ml-3 w-auto text-sm"
+				class="create"
 			>
 				Create Room
 			</button>
-			<button
-				onclick={toggle}
-				type="button"
-				class="mt-3 w-full inline-flex justify-center rounded-sm border border-gray-300 shadow-sm px-4 py-2 
-				bg-gray-50 text-base font-medium text-gray-700 hover:bg-white focus:outline-none focus:ring-2
-				focus:ring-offset-2 focus:ring-indigo-500 mt-0 ml-3 w-auto text-sm"
-			>
+			<button onclick={toggle} type="button" class="cancel">
 				Cancel
 			</button>
 		</div>
