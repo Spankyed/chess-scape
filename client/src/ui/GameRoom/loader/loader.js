62f6aea06aee3b7596ae9ae2f4a257d440f6167b
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-10-25 00:49:21 -0400

    loader proggie prior to completion

--- client/src/ui/GameRoom/loader/loader.js
+++ client/src/ui/GameRoom/loader/loader.js
@@ -1,42 +1,43 @@
 import { h } from 'hyperapp';
+import { getRandomFact } from "./randomFacts";
 
 // designs based off following code pens
 // https://codepen.io/_fbrz/pen/KvwIF
 // https://codepen.io/crayon-code/pen/eYdVLJo
 
-export default ({isLoading}) => {
+export default ({ isLoading, loaderText, setLoaderText }) => {
+	if (!loaderText) {
+		setLoaderText(getRandomFact());
+	}
 	return (
-		<div class={`mosaic absolute top-0 left-0 w-full h-full ${!isLoading && 'hidden'}`} 
-		style="background-color:#302E2B; z-index:8;">
-			<div class="relative"  style="height:50px">
-				<div id="container">
-					<div id="loader"></div>
-				</div>
+		<div class={`loader ${!true && "hide"}`}>
+				<Mosaic />
+			<div class="loader-wrapper">
+				<h2>{loaderText || "Did you know?"}</h2>
 			</div>
-
-			<div class="mosaic-loader">
-				<div class="cell d-0"></div>
-				<div class="cell d-1"></div>
-				<div class="cell d-2"></div>
-				<div class="cell d-3"></div>
-				<div class="cell d-1"></div>
-				<div class="cell d-2"></div>
-				<div class="cell d-3"></div>
-				<div class="cell d-4"></div>
-				<div class="cell d-2"></div>
-				<div class="cell d-3"></div>
-				<div class="cell d-4"></div>
-				<div class="cell d-5"></div>
-				<div class="cell d-3"></div>
-				<div class="cell d-4"></div>
-				<div class="cell d-5"></div>
-				<div class="cell d-6"></div>
-			</div>
-			<h2> LOADING </h2>
-
 		</div>
 	);
 };
 
-
-
+function Mosaic() {
+	return (
+		<div class="mosaic">
+			<div class="cell d-0"></div>
+			<div class="cell d-1"></div>
+			<div class="cell d-2"></div>
+			<div class="cell d-3"></div>
+			<div class="cell d-1"></div>
+			<div class="cell d-2"></div>
+			<div class="cell d-3"></div>
+			<div class="cell d-4"></div>
+			<div class="cell d-2"></div>
+			<div class="cell d-3"></div>
+			<div class="cell d-4"></div>
+			<div class="cell d-5"></div>
+			<div class="cell d-3"></div>
+			<div class="cell d-4"></div>
+			<div class="cell d-5"></div>
+			<div class="cell d-6"></div>
+		</div>
+	);
+}
