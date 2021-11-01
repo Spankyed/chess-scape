f643236a8aab01725fd9e7cc1b17c0f5cd39bd4f
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-11-01 13:16:22 -0400

    restyle & refactor alert css, code cleanup

--- client/src/ui/Shared/Alert.js
+++ client/src/ui/Shared/Alert.js
@@ -2,7 +2,6 @@ import { h } from 'hyperapp';
 import { nanoid } from 'nanoid/non-secure'
 import { delay } from "nanodelay";
 import debounce from "tiny-debounce";
-
 /*
 {
 	options: {
@@ -62,7 +61,7 @@ export default (initial) => ({
 	},
 	view: ({ alerts }, actions) => () => {
 		return (
-			<div class="alert-wrapper h-full absolute top-0 w-full">
+			<div class="alert-wrapper">
 				{Object.entries(alerts).map(([id, alert]) => (
 					<Alert {...{ actions, id, alert }} />
 				))}
@@ -101,44 +100,36 @@ function Alert({id, alert, actions}){
 			alert.actions?.[type].handler(result, alert.dontAskAgain)
 			close({id, completed:true})
 		}
+		const shouldCenter = !alert.actions && !alert.icon
 		return (
-			<div class={`alert ${alert.role} mx-auto flex-row justify-between`}>
+			<div class={`alert ${alert.role}`}>
 				{alert.icon && (
-					<div class="alert-icon flex items-center bg-blue-100 border-r border-blue-500 justify-center w-20 flex-shrink-0">
-						<span class="text-blue-500 px-3 h-full">
-							<img
-								class="alert-icon-img h-full"
-								src={alert.icon}
-							/>
-						</span>
+					<div class="alert-icon">
+						<img src={alert.icon} />
 					</div>
 				)}
-				<div class="alert-text m-2 mx-2 md:mx-4 flex-grow relative">
-					<div class="alert-title text-gray-200">
-						{alert.heading}
-					</div>
-					<div class="alert-message ">{alert.message}</div>
+				<div class={`alert-text ${shouldCenter && "center"}`}>
+					<div class="alert-title">{alert.heading}</div>
+					<div class="alert-message">{alert.message}</div>
 					{alert.showDAG && (
-						<label class="flex items-center absolute right-0 top-0 text-sm text-gray-500">
+						<div class="DAG">
 							<input
 								onchange={toggleAskAgain}
 								checked={alert.dontAskAgain}
 								class="form-checkbox"
 								type="checkbox"
 							/>
-							<span class="md:hidden ml-1">Remember</span>
-							<span class="hidden md:inline ml-2">
-								Don't ask again
-							</span>
-						</label>
+							<span class="remember">Remember</span>
+							<span class="dont-ask">Don't ask again</span>
+						</div>
 					)}
 				</div>
 				{alert.actions && (
-					<div class="alert-options flex-shrink flex flex-col font-semibold">
+					<div class="alert-options">
 						{alert.actions.confirm && (
 							<button
 								onclick={debounce(handleAction("confirm"), 300)}
-								class="alert-button text-blue-800 p-1 px-4"
+								class="alert-button"
 							>
 								{alert.actions.confirm.text}
 							</button>
@@ -146,7 +137,7 @@ function Alert({id, alert, actions}){
 						{alert.actions.default && (
 							<button
 								onclick={debounce(handleAction("default"), 300)}
-								class="alert-button text-gray-800 p-1 px-4"
+								class="alert-button default"
 							>
 								{alert.actions.default.text}
 							</button>
