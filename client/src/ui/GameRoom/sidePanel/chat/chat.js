3cd41878e91a0e4ef2fb9733f0d1fea1146299fa
spankyed <santiagoaj1@my.palmbeachstate.edu>
2021-11-07 18:23:30 -0500

    fix chat text area scroll showing when sidepanel not open

--- client/src/ui/GameRoom/sidePanel/chat/chat.js
+++ client/src/ui/GameRoom/sidePanel/chat/chat.js
@@ -16,91 +16,101 @@ import Api from '../../../../api/Api';
 // 	},
 // ];
 
-export default initial => ({
-	state: { 
+export default (initial) => ({
+	state: {
 		// message: '',
 		messages: [],
 	},
 
-	actions: { 
+	actions: {
 		// todo differentiate app messages
-		addMessage: (message) => state => ({ messages: [...state.messages, message] }),
+		addMessage: (message) => (state) => ({
+			messages: [...state.messages, message],
+		}),
 		// modify: (value) => value
 	},
 
-	view: (state, {addMessage}) => ({}) => {
-		var { messages } = state;
-		function init() { // ! move this into a state transition, test if chat bleeds between rooms
-			// todo: get prev chat posts, & scroll to last chat
-			// console.log('showing chat')
-			// Api.getChat(function() {});
+	view:
+		(state, { addMessage }) =>
+		({ isVisible }) => {
+			var { messages } = state;
+			function init() {
+				// ! move this into a state transition, test if chat bleeds between rooms
+				// todo: get prev chat posts, & scroll to last chat
+				// console.log('showing chat')
+				// Api.getChat(function() {});
 
-			const onLeave = ({ clientID, username }) => {
-				addMessage({text:`${username} has left the room`, appMsg: true})
+				const onLeave = ({ clientID, username }) => {
+					addMessage({
+						text: `${username} has left the room`,
+						appMsg: true,
+					});
+				};
+
+				Api.setMessageHandlers({
+					// join: onJoin,
+					leave: onLeave,
+					chat: addMessage,
+				});
+			}
+
+			var keys = {};
+			var handleKeyPress = (evt) => {
+				// console.log('keypress event',evt)
+				let { keyCode, type } = evt || Event; // to deal with IE
+				if (keyCode != 13 && keyCode != 16) return;
+				let isKeyDown = type == "keydown";
+				keys[keyCode] = isKeyDown;
+				// if key down event & enter key is pressed down & shift isn't currently being pressed down
+				if (isKeyDown && keys[13] && !keys[16]) {
+					let text = evt.target.value;
+					addMessage({
+						username: "test", // todo store user's name in Api.getUsername,
+						time: Date.now(),
+						// clientID: Api.getClientID()
+						text,
+					});
+					setTimeout((_) => (evt.target.value = ""), 1);
+					Api.sendChat(text);
+					// modify({ message: '' })
+					// todo: scrollBottom();
+				}
+				return null;
 			};
 
-			Api.setMessageHandlers({
-				// join: onJoin,
-				leave: onLeave,
-				chat: addMessage,
-			});
-		}
-
-		var keys = {}
-		var handleKeyPress = (evt) => {
-			// console.log('keypress event',evt)
-			let { keyCode, type } = evt || Event; // to deal with IE
-			if(keyCode != 13 && keyCode != 16) return
-			let isKeyDown = (type == 'keydown')
-			keys[keyCode] = isKeyDown;
-			// if key down event & enter key is pressed down & shift isn't currently being pressed down 
-			if(isKeyDown && keys[13] && !keys[16]){
-				let text = evt.target.value
-				addMessage({
-					username: 'test', // todo store user's name in Api.getUsername,
-					time: Date.now(),
-					// clientID: Api.getClientID()
-					text,
-				}) 
-				setTimeout(_=> evt.target.value = "", 1)
-				Api.sendChat(text)
-				// modify({ message: '' })
-				// todo: scrollBottom();
-			}
-			return null;
-		};
-
-		return (
-			// ! root el needs key to fix grammarly breaking textarea
-			<div oncreate={init} class="chat" key="_chat">
-				<div class="chat-window" >
-					<ul>
-						{messages.map((message, i) => (
-							<Message message={message} />
-						))}
-					</ul>
-				</div>
-				<div class="chat-footer">
-					<div class="input-wrapper">
-						<textarea
-							class="chat-input"
-							rows="2"
-							autocomplete="off"
-							placeholder="Type then press enter"
-							onkeyup={handleKeyPress}
-							onkeydown={handleKeyPress}
-							// oninput={e => modify({ message: e.target.value })}
-							// value={state.message}
-						/>
-						{/* <input class="chat-input" id="chat" placeholder="Type here and press enter" autocomplete="off"
+			return (
+				// ! root el needs key to fix grammarly breaking textarea
+				<div oncreate={init} class="chat" key="_chat">
+					<div class="chat-window">
+						<ul>
+							{messages.map((message, i) => (
+								<Message message={message} />
+							))}
+						</ul>
+					</div>
+					<div class="chat-footer">
+						<div class="input-wrapper">
+							<textarea
+								class={`chat-input ${
+									!isVisible && "not-visible"
+								}`}
+								rows="2"
+								autocomplete="off"
+								placeholder="Type then press enter"
+								onkeyup={handleKeyPress}
+								onkeydown={handleKeyPress}
+								// oninput={e => modify({ message: e.target.value })}
+								// value={state.message}
+							/>
+							{/* <input class="chat-input" id="chat" placeholder="Type here and press enter" autocomplete="off"
 								onkeyup={e => (e.keyCode === 13 ? add(e) : null)}
 							/> */}
+						</div>
 					</div>
 				</div>
-			</div>
-		);
-	}
-})
+			);
+		},
+});
 
 // function scrollBottom(){
 // 	let chatWindow = $('.chat-window')
